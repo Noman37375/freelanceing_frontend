d3d01b687a330e7434f346e7658d9a19a36fee38
Noman37375 <nomanu222555@gmail.com>
2026-03-07 11:55:40 +0500

    fix: import AsyncStorage statically for improved performance

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,11 +1,11 @@
 // utils/storage.ts
 import { Platform } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const storageSet = async (key: string, value: string) => {
   if (Platform.OS === "web") {
     localStorage.setItem(key, value);
   } else {
-    const AsyncStorage = await import("@react-native-async-storage/async-storage");
     await AsyncStorage.setItem(key, value);
   }
 };
@@ -14,7 +14,6 @@ export const storageGet = async (key: string) => {
   if (Platform.OS === "web") {
     return localStorage.getItem(key);
   } else {
-    const AsyncStorage = await import("@react-native-async-storage/async-storage");
     return await AsyncStorage.getItem(key);
   }
 };
@@ -23,7 +22,6 @@ export const storageRemove = async (key: string) => {
   if (Platform.OS === "web") {
     localStorage.removeItem(key);
   } else {
-    const AsyncStorage = await import("@react-native-async-storage/async-storage");
     await AsyncStorage.removeItem(key);
   }
 };
